1ec5bb68dac17fa60738a74e54979802089fb3b6
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-08-07 15:57:40 -0400

    added home responsive styles and mobile menu
    Signed-off-by: xJamesScott <50638563+xJamesScott@users.noreply.github.com>

---
+++ menuScripts.js
@@ -0,0 +1,13 @@
+
+const getModal = document.querySelector('.menu-modal');
+const getButton = document.querySelector('.menu-button');
+
+getButton.onclick = function () {
+    getModal.classList.toggle('active');
+}
+
+
+
+
+
+
